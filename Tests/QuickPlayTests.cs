d542431e4ba408d5c90539aeb9d1127a5e96d4f3
Katie <kaitlynnkruse@gmail.com>
2019-02-06 17:25:36 -0700

    initial commit -- deciding on archetecture

---
+++ Tests/QuickPlayTests.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class QuickPlayTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
